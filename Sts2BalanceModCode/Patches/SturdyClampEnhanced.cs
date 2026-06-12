2fa5d4cb6b7c589578f5ed847ebd959dae748523
MT-SUPER-POWER <2065838115@qq.com>
2026-06-12 10:49:32 +0800

    chore(todo): 追加了新的计划

---
+++ Sts2BalanceModCode/Patches/SturdyClampEnhanced.cs
@@ -0,0 +1,30 @@
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Relics;
+
+namespace Sts2BalanceMod.Sts2BalanceModCode.Patches;
+
+
+[HarmonyPatch(typeof(SturdyClamp), nameof(SturdyClamp.AfterPreventingBlockClear))]
+public static class SturdyClampEnhancedPatch
+{
+  private const int RetainedBlock = 20;
+
+  [HarmonyPrefix]
+  public static bool Prefix(SturdyClamp __instance, AbstractModel preventer, Creature creature, ref Task __result)
+  {
+    // 是不是当前遗物触发的保留护甲效果
+    if (__instance != preventer || creature != __instance.Owner.Creature) { return true; }
+
+    int block = creature.Block;
+    if (block != 0 && block > RetainedBlock)
+    {
+      // 替换原本逻辑的返回对象，执行我们的 Task 逻辑
+      __result = CreatureCmd.LoseBlock(creature, block - RetainedBlock);
+    }
+
+    return false;
+  }
+}
